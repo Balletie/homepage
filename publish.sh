d11f7ce6b1bc6292a245515a7d99add4b67c1faf
Balletie <skip_meesie@hotmail.com>
2015-04-06 20:09:57 +0200

    Show info during publishing. Also show local changes

--- publish.sh
+++ publish.sh
@@ -1,15 +1,24 @@
-#! /usr/local/bin/bash -x
+#! /usr/local/bin/bash
+
+function print_info {
+	echo -e "[\e[0;33minfo\e[0m] $1"
+}
+
+#./site check || exit 1;
+print_info "Showing local changes."
+git diff `git status -s | grep .*.md | cut -b4-`
+print_info "Syncing files to site."
 rsync -av --delete --exclude-from '.syncignore' _site/ ../balletie.github.io
 pushd ../balletie.github.io
-git status
-set +x
+print_info "Showing resulting change in site"
+git status -s
 if [ -z "$1" ]
 	then
 	message="`date`"
 else
 	message="$1"
 fi
-echo "Committing with message \"$message\""
+print_info "Committing with message \"$message\""
 read -p "Commit and push? (y/n) " answer
 case ${answer:0:1} in
 	y|Y)
